db81529e1545f9056dfe1fa541fc5ded4d884663
mrice2003 <a00837460@tec.mx>
2025-06-02 20:13:00 -0600

    Initial commit: Basic JWT Authentication implementation

--- src/main/java/com/example/jwt_demo/repository/UserRepository.java
+++ src/main/java/com/example/jwt_demo/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.example.jwt_demo.repository;
 import com.example.jwt_demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+ 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
